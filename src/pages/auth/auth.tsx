093b71779d3528d2643962afdc6950f8681abad1
Hein <heinminthant1999@gmail.com>
2024-04-27 00:08:29 +0700

    feat: add responsive layout

--- src/pages/auth/auth.tsx
+++ src/pages/auth/auth.tsx
@@ -3,14 +3,14 @@ import authBg from "../../assets/auth-bg.jpg";
 const AuthPage = () => {
   return (
     <div className="flex h-screen">
-      <div className="w-1/2">
+      <div className="w-1/2 hidden md:block">
         <img
           className="w-full h-full object-cover"
           src={authBg}
           alt="abstract login background art"
         />
       </div>
-      <div className="w-1/2 bg-black flex items-center justify-center px-10">
+      <div className="w-full md:w-1/2 bg-black flex items-center justify-center px-10">
         <form className="bg-white w-full max-w-xl rounded-md py-4 px-6">
           <h1 className="text-center font-bold text-3xl">Login</h1>
         </form>
