6d38e3c3a095a4884e44c9b4a2e38d017814ae12
Hein <heinminthant1999@gmail.com>
2024-04-27 00:07:25 +0700

    feat: add bg image and layout

--- src/pages/auth/auth.tsx
+++ src/pages/auth/auth.tsx
@@ -1,5 +1,22 @@
+import authBg from "../../assets/auth-bg.jpg";
+
 const AuthPage = () => {
-  return <div>This is the auth page.</div>;
+  return (
+    <div className="flex h-screen">
+      <div className="w-1/2">
+        <img
+          className="w-full h-full object-cover"
+          src={authBg}
+          alt="abstract login background art"
+        />
+      </div>
+      <div className="w-1/2 bg-black flex items-center justify-center px-10">
+        <form className="bg-white w-full max-w-xl rounded-md py-4 px-6">
+          <h1 className="text-center font-bold text-3xl">Login</h1>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AuthPage;
