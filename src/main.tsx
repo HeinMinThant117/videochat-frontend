f31448cf2b45b5f175e27acea5a44b40a86251a1
Hein <heinminthant1999@gmail.com>
2024-04-26 23:59:30 +0700

    feat: add auth page

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AuthPage from "./pages/auth/auth.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/login",
+    element: <AuthPage />,
   },
 ]);
 
---
+++ src/pages/auth/auth.tsx
@@ -0,0 +1,5 @@
+const AuthPage = () => {
+  return <div>This is the auth page.</div>;
+};
+
+export default AuthPage;
