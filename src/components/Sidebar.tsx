9b6b403c5957dd618fdf4cc11bbc6a24db1154d0
Hein <heinminthant1999@gmail.com>
2024-04-29 19:12:40 +0700

    feat: add chat sidebar responsive scroll

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,13 +4,10 @@ const Sidebar = () => {
   return (
     <div className="w-80 hidden md:block">
       <p className="font-bold border-b text-lg p-3">All Messages</p>
-      <div>
-        <SidebarItem />
-        <SidebarItem />
-        <SidebarItem />
-        <SidebarItem />
-        <SidebarItem />
-        <SidebarItem />
+      <div className=" h-[calc(100vh_-_104px)] overflow-auto">
+        {[...Array(20)].map(() => (
+          <SidebarItem />
+        ))}
       </div>
     </div>
   );
