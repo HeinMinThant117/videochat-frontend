d7ea237189e6e666e52e813158440492e80a8d86
Hein <heinminthant1999@gmail.com>
2024-05-02 18:02:19 +0700

    fix: login form max width

--- src/components/auth/AuthForm.tsx
+++ src/components/auth/AuthForm.tsx
@@ -15,12 +15,15 @@ const AuthForm = () => {
   };
 
   return (
-    <div className="w-full rounded-md bg-white px-6 py-4">
+    <div className="w-full max-w-md rounded-md bg-white px-6 py-4">
       <h1 className="mb-4 text-center text-2xl font-bold">
         {currentForm === "login" ? "Login" : "Register"}
       </h1>
       {currentForm === "login" ? <LoginForm /> : <RegisterForm />}
-      <p onClick={handleFormToggle} className="mt-2 text-center underline">
+      <p
+        onClick={handleFormToggle}
+        className="mt-2 cursor-pointer text-center underline"
+      >
         {currentForm === "login" ? "Register" : "Login"}
       </p>
     </div>
