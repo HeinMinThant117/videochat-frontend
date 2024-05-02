5d4a209cfb0ef1fa35dda2b812434846a6cddc10
Hein <heinminthant1999@gmail.com>
2024-05-02 21:05:44 +0700

    feat: add axios interceptor for request

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -5,4 +5,22 @@ const axios = Axios.create({
   timeout: 3000,
 });
 
+axios.interceptors.request.use(
+  function (config) {
+    let token = "";
+    const localStorageUser = localStorage.getItem("user");
+
+    if (localStorageUser) {
+      token = JSON.parse(localStorageUser).token;
+    }
+
+    config.headers.Authorization = `Bearer ${token}`;
+
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  },
+);
+
 export default axios;
